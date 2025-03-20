cc6d3bda0e54d4c9a4f088975966a2943ec47184
Akshara <aksharagaur0@gmail.com>
2025-03-20 16:12:12 +0530

    add component

---
+++ components/companydata.js
@@ -0,0 +1,14 @@
+import { Text,View} from 'react-native';
+
+const CompanyData=()=>{
+    return(
+        <View>
+            <Text style={{fontSize:20}}>ComName: Deqode</Text>
+            <Text style={{fontSize:20}}>comLocation: Indore</Text>
+            <Text style={{fontSize:20}}>ComEmail: xyz@deqode.com</Text>
+
+        </View>
+
+    )
+}
+export default CompanyData;
