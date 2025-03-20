e0893e65075e4f90afbe2cd8d46548ef9bf1e2c3
Akshara <aksharagaur0@gmail.com>
2025-03-20 16:31:33 +0530

    add changes

--- components/companydata.tsx
+++ components/companydata.tsx
@@ -3,6 +3,7 @@ import { Text,View} from 'react-native';
 const CompanyData=()=>{
     return(
         <View>
+            <Text style={{fontSize:25}}>Company Data</Text>
             <Text style={{fontSize:20}}>ComName: Deqode</Text>
             <Text style={{fontSize:20}}>comLocation: Indore</Text>
             <Text style={{fontSize:20}}>ComEmail: xyz@deqode.com</Text>
