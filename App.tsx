96dcef5fc93587e737defd259eb5881657b2de81
Akshara <aksharagaur0@gmail.com>
2025-03-20 16:36:34 +0530

    add comp

--- App.tsx
+++ App.tsx
@@ -6,9 +6,10 @@ import CompanyData from './components/companydata';
 const App = () => {
   return (
     <View>
-    
+      <CompanyData />
       <Text style={{fontSize:30}}>React native mobile app development</Text>
       <UserData/>
+      
     </View>
 
   )
@@ -17,7 +18,7 @@ const App = () => {
 const UserData = () =>{
   return (
     <View>
-      <Text style={{fontSize:25}}>User Data</Text>
+    <Text style={{fontSize:25}}>User Data</Text>
     <Text style={{fontSize:20}}>Name: Akshara</Text>
     <Text style={{fontSize:20}}>Age : 22</Text>
     <Text style={{fontSize:20}}>Email: akgaur@deqode.com</Text>
