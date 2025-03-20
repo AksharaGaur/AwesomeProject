279689c13f11ce32e5105f1a1efca46b695a8afb
Akshara <aksharagaur0@gmail.com>
2025-03-20 15:37:31 +0530

    add info

--- App.tsx
+++ App.tsx
@@ -1,16 +1,21 @@
-import { StyleSheet, Text, View, Button } from 'react-native'
-import React from 'react'
+import { Text, View, Button } from 'react-native';
+import React from 'react';
 
+const name="Akshara";
+var email="akgaur@deqode.com"
+let age = 22;
 const App = () => {
   return (
-    <View >
+    <View>
     
-      <Text>React native mobile app development</Text>
+      <Text style={{fontSize:30}}>React native mobile app development</Text>
       <Button title="Press Here"/>
+      <Text style={{fontSize:30}}>{name}</Text>
+      <Text style={{fontSize:30}}>{email}</Text>
+      <Text style={{fontSize:30}}>{age}</Text>
+      <Text style={{fontSize:30}}>{age>18?"Adult":"Not adult"}</Text>
     </View>
   )
 }
 
-export default App
-
-const styles = StyleSheet.create({})
+export default App;
