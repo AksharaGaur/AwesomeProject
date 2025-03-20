83b42a04a721e223fd74cc497d8061437ed8d08c
Akshara <aksharagaur0@gmail.com>
2025-03-20 16:04:15 +0530

    add userdata

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
-import { Text, View, Button } from 'react-native';
+import { Text, View} from 'react-native';
 import React from 'react';
 
-const name="Akshara";
-var email="akgaur@deqode.com"
-let age = 22;
+
 const App = () => {
   return (
     <View>
     
       <Text style={{fontSize:30}}>React native mobile app development</Text>
-      <Button title="Press Here"/>
-      <Text style={{fontSize:30}}>{name}</Text>
-      <Text style={{fontSize:30}}>{email}</Text>
-      <Text style={{fontSize:30}}>{age}</Text>
-      <Text style={{fontSize:30}}>{age>18?"Adult":"Not adult"}</Text>
+      <UserData/>
     </View>
+
+  )
+}
+
+const UserData = () =>{
+  return (
+    <View>
+      <Text style={{fontSize:25}}>User Data</Text>
+    <Text style={{fontSize:20}}>Name: Akshara</Text>
+    <Text style={{fontSize:20}}>Age : 22</Text>
+    <Text style={{fontSize:20}}>Email: akgaur@deqode.com</Text>
+    </View>
+  
   )
 }
 
