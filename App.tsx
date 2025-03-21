a1530767efd025247f44d70a7c21a2542f2b212b
Akshara <aksharagaur0@gmail.com>
2025-03-21 12:03:18 +0530

    add all changes

--- App.tsx
+++ App.tsx
@@ -1,30 +1,21 @@
 import { Text, View, Button} from 'react-native';
 import React from 'react';
 import CompanyData from './components/companydata';
+import myButton from './components/myButton';
 
+function App() {
 
-const App = () => {
-  
-  
-  
   return (
     <View>
 
-      <Text style={{fontSize:30, fontWeight:'bold', margin:10}}>React native mobile app development</Text>
+      <Text style={{ fontSize: 30, fontWeight: 'bold', margin: 10 }}>React native mobile app development</Text>
       <CompanyData />
-      <UserData/>
-      
+      <UserData />
+      <myButton />
     </View>
 
-  )
+  );
 }
-const fruit = () =>{
-  console.warn("function called")
-}
-const greet = (val : string) => {
-  console.warn(val)
-}
-
 
 const UserData = () =>{
   return (
@@ -33,8 +24,7 @@ const UserData = () =>{
     <Text style={{fontSize:20}}>Name: Akshara</Text>
     <Text style={{fontSize:20}}>Age : 22</Text>
     <Text style={{fontSize:20}}>Email: akgaur@deqode.com</Text>
-    <Button title='on Press' color={'green'} onPress={fruit}/>
-    <Button title='on Press 2' color={'blue'} onPress={(e)=>greet("Hello, Good Evening!")}/>
+    
     </View>
   
   )
---
+++ components/myButton.tsx
@@ -0,0 +1,8 @@
+
+import { Button } from 'react-native';
+
+const MyButton = () => {
+    return <Button title='Click Me' onPress={() => ('Button Pressed')}/>
+}
+
+export default MyButton;
