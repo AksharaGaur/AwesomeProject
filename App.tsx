287f2f2a9f9325831e0d455e2e91288c86cdd8c7
Akshara <aksharagaur0@gmail.com>
2025-03-21 14:14:55 +0530

    add data

--- App.tsx
+++ App.tsx
@@ -1,24 +1,15 @@
-import { Text, View, Button} from 'react-native';
+import { Text, View, Button, TouchableOpacity, StyleSheet} from 'react-native';
 import React from 'react'
 import CompanyData from './components/companydata';
 import MyButton from './components/myButton'
 import { useState } from 'react';
 
 function App() {
-  const [name, setName] = useState('Akshara')
-  let data = "Hello";
-
-  function testName(){
-    setName("Akshita")
-    data="Good Evening"
-  }
-
+  
   return (
     <View>
 
       <Text style={{ fontSize: 30, fontWeight: 'bold', margin: 10 }}>React native mobile app development</Text>
-      <Text style={{fontSize: 20}}>{name}</Text>
-      <Button title='Click Me' onPress={testName}/>
       <CompanyData />
       <UserData />
       
@@ -29,12 +20,24 @@ function App() {
 }
 
 const UserData = () =>{
+  const [name, setName] = useState('Akshara')
+  let data = "Hello";
+
+  function testName(){
+    setName("Akshita")
+  }
+
   return (
     <View>
     <Text style={{fontSize:25, fontWeight:'bold', margin:10}}>User Data</Text>
     <Text style={{fontSize:20}}>Name: Akshara</Text>
     <Text style={{fontSize:20}}>Age : 22</Text>
     <Text style={{fontSize:20}}>Email: akgaur@deqode.com</Text>
+    <Text style={{fontSize:20}}>{data}</Text>
+    <Text style={{fontSize: 20}}>{name}</Text>
+    <TouchableOpacity onPress={testName}>
+      <Text>Press Here</Text>
+    </TouchableOpacity>
     
     </View>
   
