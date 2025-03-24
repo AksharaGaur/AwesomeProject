98941eb5ad8dd5972d284316bd7decf050623839
Akshara <aksharagaur0@gmail.com>
2025-03-24 18:33:47 +0530

    add all

---
+++ components/navigation/AppNavigator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import {createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from '../screens/LoginScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+
+
